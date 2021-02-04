4554de6376440b187a217c53e0346bb060137526
lizhongxuan <zhongxuan_li@kingdee.com>
2021-02-04 18:55:22 +0800

    first commit

---
+++ kconfig.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/ghodss/yaml"
+	"github.com/tidwall/gjson"
+	"io/ioutil"
+	"os"
+)
+
+var (
+	kConfig []byte
+)
+
+func InitConfig(filePath string, isJson bool, envFilePath ...string) {
+	// 如果有环境变量,则使用环境变量路径
+	if len(envFilePath) > 0 {
+		if envpath := getEnvDf(envFilePath[0]); envpath != "" {
+			filePath = envpath
+		}
+	}
+	fmt.Println("kconfig path:", filePath)
+	// 解析yaml文件
+	file, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	if isJson { // json
+		kConfig = file
+		// check json
+		t := make(map[string]interface{})
+		if err = json.Unmarshal(kConfig, &t); err != nil {
+			panic(err)
+		}
+	} else { // yaml
+		kConfig,err = yaml.YAMLToJSON(file)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func GetString(keyPath string, df ...string) string {
+	if kConfig == nil {
+		return ""
+	}
+	v := gjson.GetBytes(kConfig, keyPath).String()
+	if v == "" && len(df) > 0 {
+		return df[0]
+	}
+	return v
+}
+
+func GetInt64(keyPath string, df ...int64) int64 {
+	if kConfig == nil {
+		return 0
+	}
+	res := gjson.GetBytes(kConfig, keyPath)
+	if !res.Exists() && len(df) > 0 {
+		return df[0]
+	}
+	return res.Int()
+}
+
+func GetStringArray(keyPath string) []string {
+	if kConfig == nil {
+		return nil
+	}
+	arr := make([]string, 0)
+	res := gjson.GetBytes(kConfig, keyPath)
+	if !res.Exists() {
+		return arr
+	}
+	t := res.Array()
+	for _, v := range t {
+		arr = append(arr, v.String())
+	}
+	return arr
+}
+
+func GetInt64Array(keyPath string) []int64 {
+	if kConfig == nil {
+		return nil
+	}
+	arr := make([]int64, 0)
+	res := gjson.GetBytes(kConfig, keyPath)
+	if !res.Exists() {
+		return arr
+	}
+	t := res.Array()
+	for _, v := range t {
+		arr = append(arr, v.Int())
+	}
+	return arr
+}
+
+func GetStringMap(keyPath string) map[string]string {
+	if kConfig == nil {
+		return nil
+	}
+	m := make(map[string]string)
+	res := gjson.GetBytes(kConfig, keyPath)
+	if !res.Exists() {
+		return m
+	}
+	t := res.Map()
+	for k, v := range t {
+		m[k] = v.String()
+	}
+	return m
+}
+
+func GetInt64Map(keyPath string) map[string]int64 {
+	if kConfig == nil {
+		return nil
+	}
+	m := make(map[string]int64)
+	res := gjson.GetBytes(kConfig, keyPath)
+	if !res.Exists() {
+		return m
+	}
+	t := res.Map()
+	for k, v := range t {
+		m[k] = v.Int()
+	}
+	return m
+}
+
+func getEnvDf(key string, df ...string) string {
+	value := os.Getenv(key)
+	if value == "" && len(df) > 0 {
+		return df[0]
+	}
+	return value
+}
---
+++ kconfig_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestYamlConfig(t *testing.T) {
+	InitConfig("test.yaml",false)
+	got := GetStringArray("arr2.arr3")
+	want := []string{"aa","bb","cc"}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got:%v, want:%v",got,want )
+	}
+
+	got2 := GetStringMap("arr.0.a")
+	want2 := map[string]string{"bb":"nnn","zz":"zzz"}
+	if !reflect.DeepEqual(got2, want2) {
+		t.Errorf("got2:%v, want2:%v",got2,want2)
+	}
+
+	got3 := GetInt64Array("arr4.arr5")
+	want3 := []int64{1,2,3}
+	if !reflect.DeepEqual(got3, want3) {
+		t.Errorf("got3:%v, want3:%v",got3,want3)
+	}
+
+	got4 := GetInt64Map("arr6")
+	want4 := map[string]int64{
+		"aa":1,
+		"bb":2,
+		"cc":3,
+	}
+	if !reflect.DeepEqual(got4, want4) {
+		t.Errorf("got4:%v, want4:%v",got4,want4)
+	}
+}
+
+func TestJsonConfig(t *testing.T) {
+	InitConfig("test.json",true)
+	got := GetInt64("bb")
+	var want int64= 11
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got:%v, want:%v",got,want )
+	}
+}
+
+func TestEnvConfig(t *testing.T) {
+	os.Setenv("testpath","test.yaml")
+	InitConfig("xxx",false,"testpath")
+	got := GetString("arr.0.a.bb")
+	want := "nnn"
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got:%v, want:%v",got,want )
+	}
+}
+
